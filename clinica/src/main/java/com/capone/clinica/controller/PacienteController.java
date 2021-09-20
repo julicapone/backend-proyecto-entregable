44d5853213680fcd662bddbd0b22cf840e9bf1a2
julietacapone <julietacapone@gmail.com>
2021-09-20 16:02:04 -0300

    agrego paciente

--- clinica/src/main/java/com/capone/clinica/controller/PacienteController.java
+++ clinica/src/main/java/com/capone/clinica/controller/PacienteController.java
@@ -1,9 +1,53 @@
 package com.capone.clinica.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.capone.clinica.model.Odontologo;
+import com.capone.clinica.model.Paciente;
+import com.capone.clinica.service.IService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+    private final IService<Paciente> pacienteService;
+
+    public PacienteController(IService<Paciente> pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Paciente> registrarPaciente(@RequestBody Paciente paciente) {
+        pacienteService.registrar(paciente);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<Paciente> actualizarPaciente(@RequestBody Paciente paciente) {
+        ResponseEntity<Paciente> response = null;
+        if (paciente.getId() != null && pacienteService.traerPorId(paciente.getId()).isPresent())
+            response = ResponseEntity.ok(pacienteService.modificar(paciente));
+        else
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return response;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> traerPaciente(@PathVariable Long id) {
+        Paciente paciente = pacienteService.traerPorId(id).orElse(null);
+        return ResponseEntity.ok(paciente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> traerPacientes(){
+        return ResponseEntity.ok(pacienteService.traerTodos());
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<?> eliminarPaciente(@PathVariable("id") Long id) {
+        pacienteService.eliminar(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
